b0d8245f6f0d160c40dc72198850ba66714c5968
Dongli Zhou <antony@bituniverse.org>
2025-05-12 20:20:16 +0800

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,29 @@
-use solana_client::rpc_client::RpcClient;
-use solana_sdk::{feature_set::tx_wide_compute_cap, signature::Signature};
-use solana_sdk::transaction::Transaction;
-use solana_sdk::pubkey::Pubkey;
-use solana_client::rpc_config::RpcTransactionConfig;
-use solana_transaction_status::{EncodedTransaction, UiTransactionEncoding, EncodedConfirmedTransactionWithStatusMeta};
+use anyhow::Result;
+use solana_client::{nonblocking::rpc_client::RpcClient, rpc_config::RpcTransactionConfig};
+use solana_sdk::{commitment_config::CommitmentConfig, signature::Signature};
+use solana_transaction_status_client_types::UiTransactionEncoding;
+use std::str::FromStr;
 
-fn main() {
-    println!("Hello, world!");
-    let rpc_client = RpcClient::new_with_commitment(
-        "http://localhost:10001".to_string(),
-        solana_sdk::commitment_config::CommitmentConfig::confirmed()
+#[tokio::main]
+async fn main() -> Result<()> {
+    let client = RpcClient::new_with_commitment(
+        String::from("http://127.0.0.1:10001"),
+        CommitmentConfig::confirmed(),
     );
 
-    // 提供待查询的交易签名
-    let signature_str = "2YYKryjHdNuvV1vkECmEHqE3XxPLwp2EhGH3GpAqZmdcv2pLT1BjoQb75QgVU7LZDB5pubqBdiWk9XSCPk1ZDrRU"; // 用实际的交易签名替换
-    let signature = signature_str.parse::<Signature>().expect("Invalid signature format");
+    let tx_sig = Signature::from_str(
+        "k263vsY8CNmAmMxynQdsfgP9sqyKJhSUfnd9CFJbtBCASSiKKWeaQd9MaA5mkVpotAKRXUaaYazoigZ3CZve67R",
+    )?;
 
-    // 设置请求配置，以Base64格式获取详细信息
     let config = RpcTransactionConfig {
-        encoding: Some(UiTransactionEncoding::Base64),
-        ..Default::default()
+        commitment: CommitmentConfig::finalized().into(),
+        encoding: UiTransactionEncoding::Base64.into(),
+        max_supported_transaction_version: Some(0),
     };
 
-    let  tx = rpc_client.get_transaction_with_config(&signature, config);
+    let transaction = client.get_transaction_with_config(&tx_sig, config).await?;
 
-    // 获取交易详情
-    match tx {
-        Ok(transaction)=> {
-            println!("Transaction details: {:?}", transaction);
-            // let jtx = transaction.transaction;
-        },
-        Err(err) => println!("Error fetching transaction: {:?}", err),
-    }
+    println!("{:#?}", transaction);
+
+    Ok(())
 }
